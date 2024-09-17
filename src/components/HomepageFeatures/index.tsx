feb24425809d5a395ff91a05f81407170ac522ff
mikera <mike@mikera.net>
2024-09-17 10:06:38 +0100

    Change home page text

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -14,8 +14,7 @@ const FeatureList: FeatureItem[] = [
     Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        Convex is the easiest way to create decentralised applications.
       </>
     ),
   },
